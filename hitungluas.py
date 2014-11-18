0500e2da25f4528212ff7369f0e0dbfd7d9c0846
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-18 11:43:34 +0700

    hitung luas

---
+++ hitungluas.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+print 'Aplikasi untuk menghitung luas segi empat'
+panjang = int(raw_input('Panjang = '))
+lebar = int(raw_input('Lebar = '))
+luas = panjang * lebar
+print 'Luas = ' + str(luas)
