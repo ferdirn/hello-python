ac68419730bf2404305b49a08415537fb1313cb5
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-19 12:30:26 +0700

    print a list

---
+++ print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+mylist = ['ayam', 'kambing', 'sapi']
+print 'mylist'
+print mylist
+
+print 'comma join'
+print ', '.join(mylist)
+
+print 'newline join'
+print '\n'.join(mylist)
+
+print 'str with strip'
+print str(mylist).strip('[]')
