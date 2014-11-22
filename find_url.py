8784a5034c4dfacca4c40f47aa2eb9947aff5f73
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-22 21:21:28 +0700

    find url

--- find_url.py
+++ find_url.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
 source = '<ul class="header-nav left" role="navigation"> <li class="header-nav-item explore"> <a class="header-nav-link" href="/explore" data-ga-click="Header, go to explore, text:explore">Explore</a>		          </li>			            <li class="header-nav-item">				                <a class="header-nav-link" href="https://gist.github.com" data-ga-click="Header, go to gist, text:gist">Gist</a>						          </li>							            <li class="header-nav-item">								                <a class="header-nav-link" href="/blog" data-ga-click="Header, go to blog, text:blog">Blog</a>										          </li>											          <li class="header-nav-item">												            <a class="header-nav-link" href="https://help.github.com" data-ga-click="Header, go to help, text:help">Help</a>													            </li> </ul>'
-print source
+#print source
+
+while True:
+    start = source.find('href="')
+    if start<0:
+        break
+    end = source.find('"', start+6)
+    url = source[start+6:end]
+    print url
+    source = source[end+1:]
