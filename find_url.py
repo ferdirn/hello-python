ddd002ffbdda9ea7d20f992b993ddd52c21a40c5
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-22 20:57:51 +0700

    add find url source

---
+++ find_url.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+source = '<ul class="header-nav left" role="navigation"> <li class="header-nav-item explore"> <a class="header-nav-link" href="/explore" data-ga-click="Header, go to explore, text:explore">Explore</a>		          </li>			            <li class="header-nav-item">				                <a class="header-nav-link" href="https://gist.github.com" data-ga-click="Header, go to gist, text:gist">Gist</a>						          </li>							            <li class="header-nav-item">								                <a class="header-nav-link" href="/blog" data-ga-click="Header, go to blog, text:blog">Blog</a>										          </li>											          <li class="header-nav-item">												            <a class="header-nav-link" href="https://help.github.com" data-ga-click="Header, go to help, text:help">Help</a>													            </li> </ul>'
+print source
