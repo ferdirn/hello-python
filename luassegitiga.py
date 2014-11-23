0968fac928b157680d42eec172e74aaff2536cb0
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-23 22:19:03 +0700

    luas segitiga with double type data

---
+++ luassegitiga.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print 'Aplikasi untuk menghitung luas segitiga'
+alas = int(raw_input('Masukkan alasnya : '))
+tinggi = int(raw_input('Masukkan tingginya : '))
+luas = alas * tinggi / 2.
+print 'Luasnya adalah ' + str(luas)
