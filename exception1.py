0bba7d96c4d3ccafe8bcf09304b0d70350b53577
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-26 16:31:21 +0700

    exception 1

---
+++ exception1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+print 'Aplikasi buka file dengan try except'
+
+try:
+    fh = open('exception1.txt', 'w')
+    fh.write('This is my test file for exception handling!')
+except IOError:
+    print 'Error: can\'t find file or read data'
+else:
+    print 'Written content in the file successfully'
+
+fh.close()
