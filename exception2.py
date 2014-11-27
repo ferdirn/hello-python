21b065d6c77a6977f4a37adc97d4e06da2776336
Ferdi Ramdhon <ferdirn@gmail.com>
2014-11-27 11:03:50 +0700

    exception 2

---
+++ exception2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+print 'Exception 2\n'
+
+try:
+    f = open('testfile', 'r')
+except IOError:
+    print 'Can\'t find or open file'
+else:
+    print 'Open file successfully'
+
