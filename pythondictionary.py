fbc825bd413bb8fa7c031db08d849a796912a61e
Ferdi Ramdhon <ferdirn@gmail.com>
2014-12-12 20:03:05 +0700

    python dictionary

---
+++ pythondictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+dict = {}
+dict['one'] = 'This is one'
+dict[2] = 'This is two'
+
+tinydict = {'name':'Ferdi', 'TTL':'22-09-1976', 'City':'Jakarta'}
+
+print dict['one']
+print dict[2]
+
