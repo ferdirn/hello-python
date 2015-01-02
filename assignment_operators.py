8259fed695c955b1940aab03a576f20b321fc37a
Ferdi Ramdhon <ferdirn@gmail.com>
2015-01-02 22:02:46 +0700

    assignment operator

---
+++ assignment_operators.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+print 'Assignment Operators'
+a = b = 20
+print 'a=',a
+print 'b=',b
+
+print 'a += 10 // a = a + 10'
+a += 10
+print a
+
+print 'a -= 5 // a = a - 5'
+a -= 5
+print a
+
+print 'a *= 3 // a = a * 3'
+a *= 3
+print a
+
+print 'a /= 5 // a = a / 5'
+a /= 5
+print a
+
+print 'a %= 2 // a = a % 2'
+b %= 2
+print b
+
+print 'a /= 5 // a = a / 5'
+a /= 5
+print a
+
+print 'a **= 3 // a = a ** 3'
+a **= 3
+print a
+
+print 'a //= 2 // a = a // 2'
+a //= 2
+print a
