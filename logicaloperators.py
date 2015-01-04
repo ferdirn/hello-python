ac8a9b54639daa50d97a22b2e597d1491bb47025
Ferdi Ramdhon <ferdirn@gmail.com>
2015-01-04 11:11:24 +0700

    logical operators

---
+++ logicaloperators.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+print 'true and true is', True and True
+print 'true and false is', True and False
+
+print 'true or true is', True or True
+print 'true or false is', True or False
+
+print 'not true is', not True
+print 'not false is', not False
