dcea3441b104f406a460e72a88995e2c809e2392
Ferdi Ramdhon <ferdirn@gmail.com>
2015-01-24 23:56:26 +0700

    create tuple vs integer

--- createtuple.py
+++ createtuple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
-tup = (2)
+tup = (2) # not a tuple but integer
+tup1 = (3,) # is tuple, add a comma in a single member of tuple
 print tup
+print type(tup) # type integer
+print tup1
+print type(tup1) # type tuple
