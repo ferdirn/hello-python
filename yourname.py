c8ff734a94359c3c6c29094c70956e5f826eda91
Ferdi Ramdhon <ferdirn@gmail.com>
2015-02-24 01:13:24 +0700

    add yourname

---
+++ yourname.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+firstname = raw_input('Your first name is ')
+lastname = raw_input('Your last name is ')
+print "Hello {} {}! Nice to see you...".format(firstname, lastname)
