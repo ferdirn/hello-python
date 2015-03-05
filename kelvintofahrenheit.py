6ee7903c84f2ae1d83807890aa6f8fb4b6ba3591
Ferdi Ramdhon <ferdirn@gmail.com>
2015-03-05 14:11:55 +0700

    catch exception in kelvin to fahrenheit

--- kelvintofahrenheit.py
+++ kelvintofahrenheit.py
@@ -4,7 +4,12 @@ def KelvinToFahrenheit(temperature):
     assert (temperature >= 0), "Colder than absolute zero!"
     return ((temperature-273)*1.8)+32
 
-print KelvinToFahrenheit(273)
-print KelvinToFahrenheit(-5)
+try:
+    print KelvinToFahrenheit(273)
+    print KelvinToFahrenheit(-5)
+except AssertionError, e:
+    print e.args
+    print str(e)
+
 
 
