0f456eda9db33f306b49335e049d2605e652d0e1
Ferdi Ramdhon <ferdirn@gmail.com>
2015-03-21 23:57:34 +0700

    python min

---
+++ python_min.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+list1, list2 = ['xyz', 'abc', 'opq'], [8, 9, 3, 5, 6]
+
+print 'list1', list1
+print 'list2', list2
+
+print 'min(list1)', min(list1)
+print 'min(list2)', min(list2)
+
+
