8eddc44d105e81d359cfaf20c52712eee43895dc
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-07 06:50:48 +0700

    sorted dict by value reverse

---
+++ sorted_dict_by_value_reverse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# regular unsorted dictionary
+d = {'banana': 3, 'apple':4, 'pear': 1, 'orange': 2}
+
+print d
+print sorted(d.items(), key=lambda t: t[1], reverse=True)
+
+
